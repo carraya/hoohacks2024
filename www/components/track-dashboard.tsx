eab859ca24139d29d690b831749ad2f78d460224
James <jamesdankwah12@gmail.com>
2024-03-23 14:34:18 -0400

    <feat> added track dashboard page to current flow

--- www/components/track-dashboard.tsx
+++ www/components/track-dashboard.tsx
@@ -1,6 +1,5 @@
-
+import { UserAuth } from "@/components/user-auth";
 import { CircleUser, Menu, Package2, Search } from "lucide-react"
-
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -23,7 +22,7 @@ import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import Link from "@/node_modules/next/link"
 
-export function Dashboard() {
+export function TrackDashboard() {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
