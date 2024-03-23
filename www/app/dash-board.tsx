da76e0ffd17a05a781592c524531a138141d91c1
James <jamesdankwah12@gmail.com>
2024-03-23 14:02:41 -0400

    <feat> added tracks page as 'dashboard'

---
+++ www/app/dash-board.tsx
@@ -0,0 +1,74 @@
+
+import { CircleUser, Menu, Package2, Search } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Checkbox } from "@/components/ui/checkbox"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import Link from "@/node_modules/next/link"
+
+export function Dashboard() {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+        <div className="flex w-full justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </header>
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+        <div className="mx-auto w-full max-w-6xl items-start gap-6 ">
+         
+          <div className="grid gap-6">
+            <Card>
+              <CardHeader>
+                <CardTitle>Follow the Prompts</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <form className="py-2">
+                  <Input placeholder="What do you know about this subject" />
+                </form>
+                <form className="py-2">
+                  <Input placeholder="What do you want to know about this subject?" />
+                </form>
+              </CardContent>
+              <CardFooter className="border-t px-6 py-4">
+                <Button>Learn</Button>
+              </CardFooter>
+            </Card>
+            
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
